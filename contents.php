cad4413b11f50eac4c0a6fd57f6bc1b3df7f51c6
Khalid Sookia <khalidsookia@gmail.com>
2015-06-22 13:28:43 +0200

    Added contents

---
+++ contents.php
@@ -0,0 +1,23 @@
+<?php
+
+$contents = array(
+    "hello" => array(
+        'auteur' => "Aurelie",
+        "titre" => "Hello",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    ),
+    "php" => array(
+        "auteur" => "Gilles",
+        "titre" => "PHP",
+        "description" => " Dolorum voluptatem magnam corporis, quaerat eum"
+    ),
+    "lorem" => array(
+        "auteur" => "Khalid",
+        "titre" => "lorem",
+        "description" => " accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe."
+    )
+);
+
+var_dump($contents);
+?>
+
